7d968d41e41d1c0f2d841d3d3774f5a78ddb9324
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2020-11-15 20:40:03 +0200

    Fixed erb template. Code is valid (https://validator.w3.org/#validate_by_upload)

--- lib/content_to_html.rb
+++ lib/content_to_html.rb
@@ -2,26 +2,26 @@
 
 require 'erb'
 
-def to_html(content, file_name = 'index', bypass_html)
+def to_html(content, bypass_html, file_name = 'index')
   bypass_html ? content : content.gsub!(%r{<([^>]+?)([^>]*?)>(.*?)</\1>}, '')
   output_file = File.expand_path("#{file_name}.html")
-  template = %(
-  <html>
+  template =%{<!DOCTYPE html>
+  <html lang="en">
   <head>
+    <meta charset="UTF-8">
     <title>Content to HTML</title>
   </head>
   <body>
     <%= @content %>
   </body>
   </html>
-  )
+  }
   @content = content
   renderer = ERB.new(template)
   result = renderer.result
-
   File.open(output_file, 'w') do |f|
     f.write(result)
   end
 end
 
-to_html('<p>Li123fe: </p>' 'ytytyt' '<p>Li12wer3fe: </p>', 'wwww', true)
+# to_html('<p>Check </p>',true)
